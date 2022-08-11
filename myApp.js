6bf84a305ef068df5e08093ed4a3cafdec1034ed
Claudia Stevens <71283725+claudiajstevens@users.noreply.github.com>
2022-08-11 05:29:07 +0000

    sending json message

--- myApp.js
+++ myApp.js
@@ -6,11 +6,22 @@ console.log("Hello World");
 /*app.get('*', function routeHandler(req, res) {
   res.send('Hello Express');
 });*/
+
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get('*', function routeHandler(req, res) {
+app.get('/', function routeHandler(req, res) {
   res.sendFile(__dirname + '/views/index.html')
 });
+
+
+app.get("/json", function(req, res) {
+  res.json({
+    "message":"Hello json"
+  });
+});
+
+
+
 
 
 
