e0bc6f25c2bb1f4d3ac5d27a69465905ef312d31
Claudia Stevens <71283725+claudiajstevens@users.noreply.github.com>
2022-08-11 21:41:47 +0000

    Created a chained middleware function

--- myApp.js
+++ myApp.js
@@ -9,12 +9,14 @@ console.log("Hello World");
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get('/', function routeHandler(req, res) {
+app.get('/', function routeHandler(req, res, next) {
   res.sendFile(__dirname + '/views/index.html')
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  //next();
 });
 
 
-app.get("/json", function(req, res) {
+app.get("/json", function(req, res, next) {
   const mySecret = process.env['MESSAGE_STYLE'];
   let message = "Hello json";
   
@@ -25,8 +27,21 @@ app.get("/json", function(req, res) {
   res.json({
     "message":message
   });
+
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  //next();
 });
 
+
+/*In the route app.get('/now', ...) chain a middleware function and the final handler. In the middleware function you should add the current time to the request object in the req.time key. You can use new Date().toString(). In the handler, respond with a JSON object, taking the structure {time: req.time}.*/
+app.get("/now", function(req, res, next){
+  req.time = new Date().toString();
+  next();
+}, function (req, res){
+    res.json({
+      "time":req.time
+    });
+});
 
 
 
