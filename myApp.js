d07dde23b7e4bff56356532417b0bafd415d47cb
Claudia Stevens <71283725+claudiajstevens@users.noreply.github.com>
2022-08-11 03:28:20 +0000

    sent an html file

--- myApp.js
+++ myApp.js
@@ -2,8 +2,13 @@ let express = require('express');
 let app = express();
 
 console.log("Hello World");
-app.get('*', function routeHandler(req, res) {
+
+/*app.get('*', function routeHandler(req, res) {
   res.send('Hello Express');
+});*/
+
+app.get('*', function routeHandler(req, res) {
+  res.sendFile(__dirname + '/views/index.html')
 });
 
 
